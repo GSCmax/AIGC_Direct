78d6e43a062e4f07ff300cdbc6b840fc532fd115
gsc <gsc067218@gmail.com>
2023-05-06 10:29:03 +0800

    add status memory

--- AIGC_Direct/App.xaml.cs
+++ AIGC_Direct/App.xaml.cs
@@ -9,20 +9,33 @@ namespace AIGC_Direct
     /// </summary>
     public partial class App : Application
     {
+        MainSprite ms;
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            var ms = new MainSprite();
+            ms = new MainSprite();
 
             ms.Opacity = 0;
 
             ms.Show();
 
-            ms.Left = 25;
-            ms.Top = 0;
+            ms.Left = Settings.Default.X;
+            ms.Top = Settings.Default.Y;
+            ms.Opacity = Settings.Default.O;
+            WindowAttach.SetIsDragElement(ms, Settings.Default.L);
+        }
 
-            ms.Opacity = 1;
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            Settings.Default.X = ms.Left;
+            Settings.Default.Y = ms.Top;
+            Settings.Default.O = ms.Opacity;
+            Settings.Default.L = WindowAttach.GetIsDragElement(ms);
+            Settings.Default.Save();
         }
     }
 }
