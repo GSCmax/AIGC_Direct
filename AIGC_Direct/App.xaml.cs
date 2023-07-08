6e463f2f27e93b4289bfea97815008cf36be9262
gsc <gsc067218@gmail.com>
2023-07-08 18:49:34 +0800

    some optimizations

--- AIGC_Direct/App.xaml.cs
+++ AIGC_Direct/App.xaml.cs
@@ -31,19 +31,31 @@ namespace AIGC_Direct
             string[] HotKey_ModifierKeys = Settings.Default.HotKey.Split(',')[0].Split('|');
             string HotKey_Key = Settings.Default.HotKey.Split(',')[1];
 
+            ModifierKeys mks = ModifierKeys.None;
+            Key k = Key.None;
+
             try
             {
-                ModifierKeys mks = ModifierKeys.None;
                 for (int i = 0; i < HotKey_ModifierKeys.Length; i++)
                     mks = mks | (ModifierKeys)Enum.Parse(typeof(ModifierKeys), HotKey_ModifierKeys[i]);
-                Key k = (Key)Enum.Parse(typeof(Key), HotKey_Key);
+                k = (Key)Enum.Parse(typeof(Key), HotKey_Key);
+            }
+            catch
+            {
+                System.Windows.Forms.Application.EnableVisualStyles();
+                System.Windows.Forms.MessageBox.Show($"Hotkey ({Settings.Default.HotKey}) is unrecognizable.\nPlease check the key names.\n\n无法识别热键({Settings.Default.HotKey})。\n请检查键名。", "AIGC Direct", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+                return;
+            }
 
+            try
+            {
                 hkh = new Helpers.HotKeyHelper(mks, k, ms, (hotkey) => { ms.Topmost = true; ms.Activate(); });
             }
             catch
             {
                 System.Windows.Forms.Application.EnableVisualStyles();
-                System.Windows.Forms.MessageBox.Show($"Hotkey ({Settings.Default.HotKey}) has already been registered.\nYou can not use hotkey to call the sprite currently.\n\n热键({Settings.Default.HotKey})已被注册。\n您当前无法使用热键呼出此工具。", "AIGC Direct", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Information);
+                System.Windows.Forms.MessageBox.Show($"Hotkey ({Settings.Default.HotKey}) has already been registered.\nYou can not use hotkey to call the sprite currently.\n\n热键({Settings.Default.HotKey})已被注册。\n您当前无法使用热键呼出此工具。", "AIGC Direct", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Warning);
+                return;
             }
         }
 
