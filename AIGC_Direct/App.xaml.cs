405f3c9ac10c0cbed67e3d8fa73cbfd25a2e50fd
gsc <gsc067218@gmail.com>
2025-02-10 18:32:36 +0800

    继续开发

--- AIGC_Direct/App.xaml.cs
+++ AIGC_Direct/App.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using AIGC_Direct.View;
 using System;
+using System.Globalization;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace AIGC_Direct
 {
@@ -17,17 +20,19 @@ namespace AIGC_Direct
         {
             base.OnStartup(e);
 
-            ms = new MainSprite();
-            ms.Opacity = 0;
-            ms.Show();
-
-            ms.Left = Settings.Default.X;
-            ms.Top = Settings.Default.Y;
-            ms.Opacity = Settings.Default.Opacity;
-            ms.Topmost = Settings.Default.Topmost;
+            #region 窗口初始化
+            ms = new MainSprite
+            {
+                Left = Settings.Default.X,
+                Top = Settings.Default.Y,
+                Opacity = Settings.Default.Opacity,
+                Topmost = Settings.Default.Topmost
+            };
             WindowAttach.SetIsDragElement(ms, Settings.Default.CanDrag);
-            //ms.Activate();
+            ms.Show();
+            #endregion
 
+            #region 热键注册
             string[] HotKey_ModifierKeys = Settings.Default.HotKey.Split(',')[0].Split('|');
             string HotKey_Key = Settings.Default.HotKey.Split(',')[1];
 
@@ -37,7 +42,7 @@ namespace AIGC_Direct
             try
             {
                 for (int i = 0; i < HotKey_ModifierKeys.Length; i++)
-                    mks = mks | (ModifierKeys)Enum.Parse(typeof(ModifierKeys), HotKey_ModifierKeys[i]);
+                    mks |= (ModifierKeys)Enum.Parse(typeof(ModifierKeys), HotKey_ModifierKeys[i]);
                 k = (Key)Enum.Parse(typeof(Key), HotKey_Key);
             }
             catch
@@ -57,20 +62,186 @@ namespace AIGC_Direct
                 System.Windows.Forms.MessageBox.Show($"Hotkey ({Settings.Default.HotKey}) has already been registered.\nYou can not use hotkey to call the sprite currently.\n\n热键({Settings.Default.HotKey})已被注册。\n您当前无法使用热键呼出此工具。", "AIGC Direct", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Warning);
                 return;
             }
+            #endregion
         }
 
         protected override void OnExit(ExitEventArgs e)
         {
             base.OnExit(e);
 
-            Settings.Default.X = ms.Left;
+            Settings.Default.X = ms!.Left;
             Settings.Default.Y = ms.Top;
             Settings.Default.Opacity = Math.Round(ms.Opacity, 2);
             Settings.Default.Topmost = ms.Topmost;
             Settings.Default.CanDrag = WindowAttach.GetIsDragElement(ms);
             Settings.Default.Save();
 
-            hkh.Dispose();
+            hkh!.Dispose();
         }
     }
+
+    #region Converters
+    public class BorderClipConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 3 && values[0] is double width && values[1] is double height && values[2] is CornerRadius radius)
+            {
+                if (width < double.Epsilon || height < double.Epsilon)
+                {
+                    return Geometry.Empty;
+                }
+
+                var clip = new PathGeometry
+                {
+                    Figures = new PathFigureCollection
+                {
+                    new(new Point(radius.TopLeft, 0), new PathSegment[]
+                    {
+                        new LineSegment(new Point(width - radius.TopRight, 0), false),
+                        new ArcSegment(new Point(width, radius.TopRight), new Size(radius.TopRight, radius.TopRight), 90, false, SweepDirection.Clockwise, false),
+                        new LineSegment(new Point(width, height - radius.BottomRight), false),
+                        new ArcSegment(new Point(width - radius.BottomRight, height), new Size(radius.BottomRight, radius.BottomRight), 90, false, SweepDirection.Clockwise, false),
+                        new LineSegment(new Point(radius.BottomLeft, height), false),
+                        new ArcSegment(new Point(0, height - radius.BottomLeft), new Size(radius.BottomLeft, radius.BottomLeft), 90, false, SweepDirection.Clockwise, false),
+                        new LineSegment(new Point(0, radius.TopLeft), false),
+                        new ArcSegment(new Point(radius.TopLeft, 0), new Size(radius.TopLeft, radius.TopLeft), 90, false, SweepDirection.Clockwise, false),
+                    }, false)
+                }
+                };
+                clip.Freeze();
+
+                return clip;
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    public class Bool2ResourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return Application.Current.FindResource(((string)parameter).Split(',')[0]);
+            else
+                return Application.Current.FindResource(((string)parameter).Split(',')[1]);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class IconShowConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)values[0]
+                || (bool)values[1]
+                || (Visibility)values[2] == Visibility.Visible
+                || (Visibility)values[3] == Visibility.Visible
+                || (Visibility)values[4] == Visibility.Visible
+                || (Visibility)values[5] == Visibility.Visible
+                || (Visibility)values[6] == Visibility.Visible)
+                return Visibility.Visible;
+            else return Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class WebViewControlShowConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Visibility)values[0] == Visibility.Visible
+                || (Visibility)values[1] == Visibility.Visible
+                || (Visibility)values[2] == Visibility.Visible
+                || (Visibility)values[3] == Visibility.Visible
+                || (Visibility)values[4] == Visibility.Visible)
+                return Visibility.Visible;
+            else return Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ExitIconShowConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)values[0]
+                && (Visibility)values[1] != Visibility.Visible
+                && (Visibility)values[2] != Visibility.Visible
+                && (Visibility)values[3] != Visibility.Visible
+                && (Visibility)values[4] != Visibility.Visible
+                && (Visibility)values[5] != Visibility.Visible)
+                return Visibility.Visible;
+            else return Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CornerRadiusSelConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Visibility)values[0] != Visibility.Visible
+                && (Visibility)values[1] != Visibility.Visible
+                && (Visibility)values[2] != Visibility.Visible
+                && (Visibility)values[3] != Visibility.Visible
+                && (Visibility)values[4] != Visibility.Visible)
+                return new CornerRadius(0, 24, 24, 24);
+            else return new CornerRadius(0, 0, 0, 24);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Link2FaviconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"https://www.google.com/s2/favicons?domain={value}&sz=40";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Value2HalfConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value / 2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
 }
--- AIGC_Direct/View/MainSprite.xaml.cs
+++ AIGC_Direct/View/MainSprite.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Globalization;
+﻿using Microsoft.Web.WebView2.Wpf;
+using System;
 using System.Windows;
-using System.Windows.Data;
 using System.Windows.Input;
-using System.Windows.Media;
 
 namespace AIGC_Direct.View
 {
@@ -12,12 +10,14 @@ namespace AIGC_Direct.View
     /// </summary>
     public partial class MainSprite : Window
     {
+        WebView2? nowWV;
+
         public MainSprite()
         {
             InitializeComponent();
         }
 
-        private void link1_Click(object sender, RoutedEventArgs e)
+        private void Link1_Click(object sender, RoutedEventArgs e)
         {
             if (wv1.Visibility == Visibility.Visible)
             {
@@ -34,10 +34,11 @@ namespace AIGC_Direct.View
                 wv3.Visibility = Visibility.Collapsed;
                 wv4.Visibility = Visibility.Collapsed;
                 wv5.Visibility = Visibility.Collapsed;
+                nowWV = wv1;
             }
         }
 
-        private void link2_Click(object sender, RoutedEventArgs e)
+        private void Link2_Click(object sender, RoutedEventArgs e)
         {
             if (wv2.Visibility == Visibility.Visible)
             {
@@ -54,10 +55,11 @@ namespace AIGC_Direct.View
                 wv3.Visibility = Visibility.Collapsed;
                 wv4.Visibility = Visibility.Collapsed;
                 wv5.Visibility = Visibility.Collapsed;
+                nowWV = wv2;
             }
         }
 
-        private void link3_Click(object sender, RoutedEventArgs e)
+        private void Link3_Click(object sender, RoutedEventArgs e)
         {
             if (wv3.Visibility == Visibility.Visible)
             {
@@ -74,10 +76,11 @@ namespace AIGC_Direct.View
                 wv3.Visibility = Visibility.Visible;
                 wv4.Visibility = Visibility.Collapsed;
                 wv5.Visibility = Visibility.Collapsed;
+                nowWV = wv3;
             }
         }
 
-        private void link4_Click(object sender, RoutedEventArgs e)
+        private void Link4_Click(object sender, RoutedEventArgs e)
         {
             if (wv4.Visibility == Visibility.Visible)
             {
@@ -94,10 +97,11 @@ namespace AIGC_Direct.View
                 wv3.Visibility = Visibility.Collapsed;
                 wv4.Visibility = Visibility.Visible;
                 wv5.Visibility = Visibility.Collapsed;
+                nowWV = wv4;
             }
         }
 
-        private void link5_Click(object sender, RoutedEventArgs e)
+        private void Link5_Click(object sender, RoutedEventArgs e)
         {
             if (wv5.Visibility == Visibility.Visible)
             {
@@ -114,40 +118,16 @@ namespace AIGC_Direct.View
                 wv3.Visibility = Visibility.Collapsed;
                 wv4.Visibility = Visibility.Collapsed;
                 wv5.Visibility = Visibility.Visible;
+                nowWV = wv5;
             }
         }
 
-        private void link1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            wv1.CoreWebView2.Navigate(Settings.Default.link1);
-        }
-
-        private void link2_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            wv2.CoreWebView2.Navigate(Settings.Default.link2);
-        }
-
-        private void link3_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            wv3.CoreWebView2.Navigate(Settings.Default.link3);
-        }
-
-        private void link4_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            wv4.CoreWebView2.Navigate(Settings.Default.link4);
-        }
-
-        private void link5_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            wv5.CoreWebView2.Navigate(Settings.Default.link5);
-        }
-
-        private void info_Click(object sender, RoutedEventArgs e)
+        private void Info_Click(object sender, RoutedEventArgs e)
         {
             Topmost = !Topmost;
         }
 
-        private void info_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        private void Info_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             if (WindowAttach.GetIsDragElement(this))
                 WindowAttach.SetIsDragElement(this, false);
@@ -155,161 +135,33 @@ namespace AIGC_Direct.View
                 WindowAttach.SetIsDragElement(this, true);
         }
 
-        private void info_MouseWheel(object sender, MouseWheelEventArgs e)
+        private void Info_MouseWheel(object sender, MouseWheelEventArgs e)
         {
-            var o = Math.Round(this.Opacity, 2);
+            var o = Math.Round(Opacity, 2);
             if (e.Delta > 0)
             {
                 if (o < 1.0)
-                    this.Opacity += 0.05;
+                    Opacity += 0.05;
                 else
-                    this.Opacity = 1.0;
+                    Opacity = 1.0;
             }
             else
             {
                 if (o > 0.5)
-                    this.Opacity -= 0.05;
+                    Opacity -= 0.05;
                 else
-                    this.Opacity = 0.5;
+                    Opacity = 0.5;
             }
         }
 
-        private void exit_Click(object sender, RoutedEventArgs e)
+        private void Exit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
-    }
 
-    #region Converters
-    public class BorderClipConverter : IMultiValueConverter
-    {
-        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        private void Restore_Click(object sender, RoutedEventArgs e)
         {
-            if (values.Length == 3 && values[0] is double width && values[1] is double height && values[2] is CornerRadius radius)
-            {
-                if (width < double.Epsilon || height < double.Epsilon)
-                {
-                    return Geometry.Empty;
-                }
-
-                var clip = new PathGeometry
-                {
-                    Figures = new PathFigureCollection
-                {
-                    new(new Point(radius.TopLeft, 0), new PathSegment[]
-                    {
-                        new LineSegment(new Point(width - radius.TopRight, 0), false),
-                        new ArcSegment(new Point(width, radius.TopRight), new Size(radius.TopRight, radius.TopRight), 90, false, SweepDirection.Clockwise, false),
-                        new LineSegment(new Point(width, height - radius.BottomRight), false),
-                        new ArcSegment(new Point(width - radius.BottomRight, height), new Size(radius.BottomRight, radius.BottomRight), 90, false, SweepDirection.Clockwise, false),
-                        new LineSegment(new Point(radius.BottomLeft, height), false),
-                        new ArcSegment(new Point(0, height - radius.BottomLeft), new Size(radius.BottomLeft, radius.BottomLeft), 90, false, SweepDirection.Clockwise, false),
-                        new LineSegment(new Point(0, radius.TopLeft), false),
-                        new ArcSegment(new Point(radius.TopLeft, 0), new Size(radius.TopLeft, radius.TopLeft), 90, false, SweepDirection.Clockwise, false),
-                    }, false)
-                }
-                };
-                clip.Freeze();
-
-                return clip;
-            }
-
-            return DependencyProperty.UnsetValue;
-        }
-
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-        {
-            throw new NotSupportedException();
+            nowWV!.CoreWebView2.Navigate((string)nowWV.Tag);
         }
     }
-
-    public class Bool2ResourceConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if ((bool)value)
-                return Application.Current.FindResource(((string)parameter).Split(',')[0]);
-            else
-                return Application.Current.FindResource(((string)parameter).Split(',')[1]);
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class IconShowConverter : IMultiValueConverter
-    {
-        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-        {
-            if ((bool)values[0]
-                || (bool)values[1]
-                || (Visibility)values[2] == Visibility.Visible
-                || (Visibility)values[3] == Visibility.Visible
-                || (Visibility)values[4] == Visibility.Visible
-                || (Visibility)values[5] == Visibility.Visible
-                || (Visibility)values[6] == Visibility.Visible)
-                return Visibility.Visible;
-            else return Visibility.Collapsed;
-        }
-
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class ExitIconShowConverter : IMultiValueConverter
-    {
-        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-        {
-            if ((bool)values[0]
-                && (Visibility)values[1] != Visibility.Visible
-                && (Visibility)values[2] != Visibility.Visible
-                && (Visibility)values[3] != Visibility.Visible
-                && (Visibility)values[4] != Visibility.Visible
-                && (Visibility)values[5] != Visibility.Visible)
-                return Visibility.Visible;
-            else return Visibility.Collapsed;
-        }
-
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class CornerRadiusSelConverter : IMultiValueConverter
-    {
-        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-        {
-            if ((Visibility)values[0] != Visibility.Visible
-                && (Visibility)values[1] != Visibility.Visible
-                && (Visibility)values[2] != Visibility.Visible
-                && (Visibility)values[3] != Visibility.Visible
-                && (Visibility)values[4] != Visibility.Visible)
-                return new CornerRadius(0, 24, 24, 24);
-            else return new CornerRadius(0, 0, 0, 24);
-        }
-
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class Link2FaviconConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return $"https://www.google.com/s2/favicons?domain={value}&sz=40";
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-    #endregion
 }
